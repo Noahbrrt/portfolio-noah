6f224c156a4a6d4f79e681ddd8e288afe6d914c7
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2025-11-19 21:42:00 +0100

    Pb.ts

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -3,6 +3,6 @@ import type { TypedPocketBase } from "./pocketbase-types";
 var path='';
 if(import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'    //localhost = machine de dev
-else path = 'http://localhost:8095'   //localhost = machine de déploiement
+else path = 'https://portfolio.noah-barret.fr'   //localhost = machine de déploiement
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
