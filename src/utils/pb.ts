9fa3d6ed1f0a9f8a30b3cd3d5ac48419f1823a47
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2025-11-19 11:29:20 +0100

    portfolio

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,8 +1,8 @@
-import PocketBase from "pocketbase";
-
-const PB_URL =
-  import.meta.env.PUBLIC_PB_URL || "http://127.0.0.1:8090/";
-
-const pb = new PocketBase(PB_URL);
-
+import PocketBase from 'pocketbase';
+import type { TypedPocketBase } from "./pocketbase-types";
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    //localhost = machine de dev
+else path = 'http://localhost:8095'   //localhost = machine de déploiement
+const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
